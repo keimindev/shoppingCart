5eb53b41759e82d6146c5ba1254d1518f833c6b3
min <keiramingj@gmail.com>
2021-07-12 16:39:09 +0900

    Renew 1.Add product to Cart / Cart Notification / Delete All / Reload product from Cart

---
+++ js/function.js
@@ -0,0 +1,208 @@
+'use strict';
+
+//Elements
+const content = document.querySelector('.product');
+const orderBox = document.querySelector('.shopping-detail');
+const orderList = document.querySelector('.order-box');
+const totalPrice = document.querySelector('.total-price');
+const overlay = document.querySelector('.overlay');
+const cartBtn = document.querySelector('.cart');
+const closeList = document.querySelector('.close');
+const deleteAllBtn = document.querySelector('.deleteall-btn');
+
+let buttons = [];
+let item, itemPrice, itemImg, itemAmount;
+let productOrder = [];
+
+
+//UI - Get Donuts from Array
+let results = "";
+
+
+Food.forEach(({
+  name,
+  img,
+  price,
+  id
+}) => {
+  results += `<div class="product-box">
+                <div class="product-img">
+                  <img src="${img}" alt""/>
+                </div>
+              <div class="product-info">
+              <span></span>
+              <p class="name">${name}</p>
+              <p class="price">₩${price}</p>
+              </div>
+              <div class="add-btn" value=${id-1}>+ Add</div>
+              </div>`
+})
+
+content.innerHTML = results;
+
+//Add To Cart
+const addBtns = [...document.querySelectorAll('.add-btn')];
+buttons = addBtns;
+
+for (let i = 0; i < addBtns.length; i++) {
+  addBtns[i].addEventListener('click', () => {
+    cartNumbers(Food[i]);
+    totalCoast(Food[i]);
+    displayOrder(Food[i]);
+
+  })
+}
+
+
+function onLoadCartNumber() {
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  if (productNumbers) {
+    cartBtn.setAttribute('data-count', productNumbers);
+    cartBtn.classList.add('add-numb');
+  }
+
+
+}
+
+
+function cartNumbers(product) {
+  console.log(product)
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1)
+    cartBtn.setAttribute('data-count', productNumbers + 1);
+    cartBtn.classList.add('add-numb');
+  } else {
+    localStorage.setItem('cartNumbers', 1);
+    cartBtn.setAttribute('data-count', 1);
+    cartBtn.classList.add('add-numb');
+  }
+
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+
+    if (cartItems[product.id] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.id]: product
+      }
+    }
+    cartItems[product.id].inCart += 1;
+  } else {
+    product.inCart = 1;
+
+    cartItems = {
+      [product.id]: product
+    }
+  }
+
+  localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+}
+
+
+function totalCoast(product) {
+  let cartCost = JSON.parse(localStorage.getItem('totalCost'));
+
+  if (cartCost != null) {
+    localStorage.setItem('totalCost', cartCost + product.price);
+  } else {
+    localStorage.setItem('totalCost', product.price);
+  }
+
+
+}
+
+function displayOrder(product){
+  const div = document.createElement('div');
+  div.classList.add('order-list');
+    div.innerHTML = '';
+      div.innerHTML += `<div><div class="order-food">
+    <img src="${product.img}">
+    </div>
+    <div class="order-info">
+    <p class="order-product">${product.name}</p>
+    <p class="order-price">₩${product.price * (product.inCart+1)}</p>
+    </div>
+    <div class="order-amount">
+    <p class="countUp xi-caret-up-min"></p>
+    <span class="item-amount">${product.inCart}</span>
+    <p class="countDown xi-caret-down-min"></p>
+    </div>
+    <div><i class="xi-trash"></i></div></div>
+    `;
+
+    orderList.appendChild(div);
+
+  let cost = JSON.parse(localStorage.getItem('totalCost'));
+  totalPrice.innerHTML = cost.toLocaleString(); 
+}
+
+
+//reload cart, display cart ui
+function displayCart() {
+  let cartItem = JSON.parse(localStorage.getItem('productsInCart'));
+  const div = document.createElement('div');
+  div.classList.add('order-list');
+
+  if (cartItem && orderList) {
+    div.innerHTML = '';
+    Object.values(cartItem).map(item => {
+      div.innerHTML += `<div><div class="order-food">
+    <img src="${item.img}">
+    </div>
+    <div class="order-info">
+    <p class="order-product">${item.name}</p>
+    <p class="order-price">₩${item.price * item.inCart}</p>
+    </div>
+    <div class="order-amount">
+    <p class="countUp xi-caret-up-min"></p>
+    <span class="item-amount">${item.inCart}</span>
+    <p class="countDown xi-caret-down-min"></p>
+    </div>
+    <div><i class="xi-trash"></i></div></div>
+    `;
+    })
+
+    orderList.appendChild(div);
+
+  }
+
+  let cost = JSON.parse(localStorage.getItem('totalCost'));
+  totalPrice.innerHTML = cost.toLocaleString();
+
+}
+
+//show OrerList
+cartBtn.addEventListener("click", () => {
+  orderBox.classList.add('active');
+  overlay.classList.add('active');
+})
+
+//Close OderList
+closeList.addEventListener("click", () => {
+  orderBox.classList.remove('active');
+  overlay.classList.remove('active');
+})
+
+//delte
+deleteAllBtn.addEventListener('click', () =>{
+  orderList.remove();
+  cartBtn.classList.remove('add-numb');
+  localStorage.removeItem('totalCost');
+  localStorage.removeItem('cartNumbers');
+  localStorage.removeItem('productsInCart');
+})
+
+onLoadCartNumber();
+displayCart();
+
---
+++ js/products.js
@@ -0,0 +1,98 @@
+let Food = [
+    {
+        id: 1,
+        tag: 'donuts',
+        name: 'Diget Milk Cream Tart',
+        price: 1300,
+        img: 'img/product-1.jpg',
+        inCart: 0
+    },
+    {
+        id: 2,
+        tag: 'donuts',
+        name: 'Famous Glazed',
+        price: 1150,
+        img: 'img/product-2.jpg',
+        inCart: 0
+    },
+    {
+        id: 3,
+        tag: 'donuts',
+        name: 'Boston Kreme',
+        price: 1200,
+        img: 'img/product-3.jpg',
+        inCart: 0
+    },
+    {
+        id: 4,
+        tag: 'donuts',
+        name: 'Dual Fillng Heart',
+        price: 1250,
+        img: 'img/product-4.jpg',
+        inCart: 0
+    },
+    {
+        id: 5,
+        tag: 'donuts',
+        name: 'Strawberry Filled',
+        price: 1150,
+        img: 'img/product-5.jpg',
+        inCart: 0
+    },
+    {
+        id: 6,
+        tag: 'donuts',
+        name: 'Diget Crumble Choco',
+        price: 1300,
+        img: 'img/product-6.jpg',
+        inCart: 0
+    },
+    {
+        id: 7,
+        tag: 'Muffin',
+        name: 'HERSHEY´s Choco Muffin',
+        price: 2100,
+        img: 'img/product-7.jpg',
+        inCart: 0
+    },
+    {
+        id: 8,
+        tag: 'Bagel',
+        name: 'Blueberry Bagel',
+        price: 1300,
+        img: 'img/product-8.jpg',
+        inCart: 0
+    },
+    {
+        id: 9,
+        tag: 'Coffee',
+        name: 'Butter Scotch Latte(S)',
+        price: 3400,
+        img: 'img/product-9.jpg',
+        inCart: 0
+    },
+    {
+        id: 10,
+        tag: 'Coffee',
+        name: 'Americano(S)',
+        price: 2000,
+        img: 'img/product-10.jpg',
+        inCart: 0
+    },
+    {
+        id: 11,
+        tag: 'Coffee',
+        name: 'Cafe latte(S)',
+        price: 2400,
+        img: 'img/product-11.jpg',
+        inCart: 0
+    },
+    {
+        id: 12,
+        tag: 'Coffee',
+        name: 'Cold Brew Condensed latte',
+        price: 3600,
+        img: 'img/product-12.jpg',
+        inCart: 0
+    }
+]
